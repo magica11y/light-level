911ecfa023785f5d6582425da4900de45d9ef350
Rishabh Rao <rishabhsrao@gmail.com>
2019-07-24 14:15:25 +0200

    :tada: Initialize repository by 'referencing' magica11y/prefers-reduced-motion

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// @flow
+
+import lightLevel from './lightLevel';
+import availableLightLevels, { type LightLevel } from './availableLightLevels';
+
+export default lightLevel;
+
+export { availableLightLevels };
+export type { LightLevel };
---
+++ src/lightLevel.js
@@ -0,0 +1,32 @@
+// @flow
+
+import matchUserPreference from '@magica11y/cauldron';
+
+import availableLightLevels, { type LightLevel } from './availableLightLevels';
+
+const lightLevelsValues: Array<LightLevel> = [
+  availableLightLevels.NORMAL,
+  availableLightLevels.DIM,
+  availableLightLevels.WASHED,
+];
+
+/**
+ * Detects the ambient light-level in which the device is used
+ * using CSS3 Media Queries level 5 specification for `'light-level'`.
+ *
+ * @returns Either 'normal', 'dim', 'washed' or `null`
+ * @see https://drafts.csswg.org/mediaqueries-5/#light-level
+ */
+const lightLevel = (): ?LightLevel => {
+  const matchedLightLevel: ?LightLevel = lightLevelsValues.find((lightLevelValue: LightLevel) =>
+    matchUserPreference('light-level', lightLevelValue),
+  );
+
+  if (matchedLightLevel) {
+    return matchedLightLevel;
+  }
+
+  return null;
+};
+
+export default lightLevel;
